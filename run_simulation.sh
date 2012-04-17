272e1c7ff554fe57e1bcd2cd01243b5eb6c9334a
Kris Samala <mksamala@gmail.com>
2012-04-17 11:41:35 -0400

    Update value ranges

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 count=1
-for p in `seq 0.001 .001 .1`; do
-    for g in `seq 1 1 5`; do
-        for s in `seq .01 .01 1`; do
-            ./runCmd -c "python simulation.py 52 1 100000 $p $g $s out/locations.p citydata/census.p out/gowalla_net results/sim$count results/map$count" -o results/out$count.txt -e results/out$count.err --nowait
+for p in `seq 0.01 .01 .5`; do
+    for g in `seq .5 .5 3`; do
+        for s in `seq .1 .1 1`; do
+            ./runCmd -c "python simulation.py 52 1 500000 $p $g $s out/locations.p citydata/census.p out/gowalla_net results/sim$count results/map$count" -o results/out$count.txt -e results/out$count.err --nowait
             let count+=1
         done
     done

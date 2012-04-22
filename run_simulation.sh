5b8e314fcd7e327929d20b5653e8b4bdcf5cee3b
Kris Samala <mksamala@gmail.com>
2012-04-21 22:25:27 -0400

    fixed syntax error

--- run_simulation.sh
+++ run_simulation.sh
@@ -4,7 +4,7 @@ count=1
 for p in `seq 0.01 .01 .4`; do
     for k in `seq 1 1 10`; do
         for s in `seq .5 .5 5`; do
-            for t in `seq 1 1 10` do
+            for t in `seq 1 1 10`; do
                 ./runCmd -c "python simulation.py 100000000 $p $k $s citydata/states.p citydata/census.p out/gowalla_net results/matrix$count-$t" -e err/out$count.err --nowait
             done
             let count+=1

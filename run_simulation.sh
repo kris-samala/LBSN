6626890564e0693a582d1a5cbd2aff0ddb87e488
Kris Samala <mksamala@gmail.com>
2012-05-02 01:29:11 -0400

    fixed the problem of not running

--- run_simulation.sh
+++ run_simulation.sh
@@ -3,8 +3,7 @@
 count=1
 for p in `seq 0.01 .01 .4`; do
     for t in `seq 1 1 10`; do
-        ./runCmd -c "python sim.py 500000 $p out/contact_dist.out
-        citydata/states.p citydata/census.p out/trans_prob.csv out/city_list.p results/matrix$count-$t" -e err/out$count-$t.err --nowait
+        ./runCmd -c "python sim.py 500000 $p out/contact_dist.out citydata/states.p citydata/census.p out/trans_prob.csv out/city_list.p results/matrix$count-$t" -e err/out$count-$t.err --nowait
     done
     let count+=1
 done

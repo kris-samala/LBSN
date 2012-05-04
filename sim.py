9decdc0313109eafe52363369321d387c3c2186e
Kris Samala <mksamala@gmail.com>
2012-05-03 21:54:48 -0400

    removed print

--- sim.py
+++ sim.py
@@ -90,7 +90,6 @@ def gen_latent(city, l):
 def init_city(city, l):
     frac = .0001
     num = int(frac * population[city])
-    print num
     for i in range(num):
         l.update(gen_latent(city, l))
 

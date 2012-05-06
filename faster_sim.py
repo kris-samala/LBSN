523e67160dae973e5ac91b8047189c66be0a2ca7
Kris Samala <mksamala@gmail.com>
2012-05-05 23:19:04 -0400

    deterministic movement

---
+++ faster_sim.py
@@ -0,0 +1,174 @@
+from util2 import Util
+import numpy as np
+import fileinput
+import random
+import pickle
+import csv
+import sys
+
+#python faster_sim.py [n] [prob] contact_dist.out states.p census.p trans_prob.csv city_list.p [matrix.out]
+
+
+#setup parameter values
+time_steps = 180
+n = int(sys.argv[1])
+beta = float(sys.argv[2])
+max_inf = 10
+max_lat = 3
+
+#load contact distribution
+contacts = []
+for line in fileinput.input(sys.argv[3]):
+    line = line.rstrip(',').split(",")
+    line = [int(x) for x in line]
+    contacts.extend(line)
+
+states = pickle.load(open(sys.argv[4], 'rb'))
+
+#load census data
+population = {}
+census = pickle.load(open(sys.argv[5], 'rb'))
+total_pop = 0
+for p in census:
+    total_pop += census[p]
+
+for p in census:
+    population[p] = int((census[p] / float(total_pop)) * n)
+
+#load transition probability matrix
+reader = csv.reader(open(sys.argv[6], 'rb'), delimiter=',')
+T =[]
+for row in reader:
+    row = [(float(x) if x else 0) for x in row]
+    T.append(np.array(row))
+
+T = np.array(T)
+city_list = pickle.load(open(sys.argv[7], 'rb'))
+num_cities = len(city_list)
+
+print "Dictionaries loaded."
+
+#output file
+params = str(beta)
+matrix = open(sys.argv[8]+"_"+params+".out", 'w')
+
+#initialize susceptible vector
+Vs = []
+for i in range(num_cities):
+    Vs.append(population[city_list[i]])
+Vs = np.array([Vs])
+
+#initialize latent vectors
+Vl = Util.init_vectors(max_lat, num_cities)
+
+#initialize infected vectors
+Vi = Util.init_vectors(max_inf, num_cities)
+
+print "Vectors initialized."
+
+#initial infected city
+
+init_city = "New York,NY"
+init_index = city_list.index(init_city)
+init_pop = int(.001 * Vs[0,init_index])
+print "Initial infected population " + str(init_pop)
+Vs[0,init_index] -= init_pop
+
+for i in range(init_pop):
+    length = random.randint(1, max_inf)
+    v = Vi[length-1]
+    v[0,init_index] += 1
+
+print "Initial city outbreak.. Done."
+print "Begin simulation..."
+
+t = 0
+infection_matrix = []
+
+while t < time_steps:
+    infection_matrix.append(Util.state_stats(city_list,states,Vi))
+
+    print "Spreading infection..."
+
+    #spread infection
+    Vnl = []
+    for i in range(0,max_lat):
+        Vnl.append( np.zeros(num_cities) )
+    Vnl = np.array(Vnl)
+
+    for i in range(len(Vi)):
+        v = Vi[i]
+        for r in range(v.shape[0]):
+            temp = []
+            for c in range(v.shape[1]):
+                Vnl = Util.propagate(v[r,c], c, Vnl, beta, contacts, max_lat, t)
+
+    print "Updating compartments..."
+
+    #update latent
+    new_infected = []
+    for i in range(len(Vl)):
+        v = Vl[i]
+        rows = v.shape[0]
+        for r in reversed(range(rows)):
+            #last row becomes infected at next step
+            if r == rows-1:
+                new_infected.append(v[r])
+            if r == 0:
+                v[r] = Vnl[i]
+            else:
+                v[r] = v[r-1]
+
+    new_infected = np.sum(new_infected, axis=0)
+
+    print "New infected " + str(np.sum(new_infected))
+
+    #determine infection period for newly infected
+    Vni = []
+    for i in range(0, max_inf):
+        Vni.append( np.zeros(num_cities) )
+    Vni = np.array(Vni)
+
+    Vni = Util.determine_inf_pd(new_infected, Vni, max_inf)
+
+    #update infected
+    for i in range(len(Vi)):
+        v = Vi[i]
+        rows = v.shape[0]
+        for r in reversed(range(rows)):
+            if r == 0:
+                v[r] = Vni[i]
+            else:
+                v[r] = v[r-1]
+
+    print "Mobilizing agents..."
+
+    #latents move to new city
+    for i in range(len(Vl)):
+        v = Vl[i]
+        for r in range(v.shape[0]):
+            temp = []
+            for c in range(v.shape[1]):
+                temp.append( Util.det_distribute(v[r,c], T[c,:]) )
+            v[r] = np.sum(temp,axis=0)
+
+    #infected move to new city
+    for i in range(len(Vi)):
+        v = Vi[i]
+        for r in range(v.shape[0]):
+            temp = []
+            for c in range(v.shape[1]):
+                if v[r,c] > 0:
+                    temp.append( Util.det_distribute(v[r,c], T[c,:]) )
+            v[r] = np.sum(temp,axis=0)
+
+    t += 1
+
+
+#write output to file
+for row in infection_matrix:
+    matrix.write(str(row)+'\n')
+
+matrix.close()
+
+print "Simulation finished."
---
+++ util2.py
@@ -0,0 +1,95 @@
+import numpy as np
+import random
+import math
+
+class Util:
+
+    @staticmethod
+    def state_stats(city_list, states, inf_vectors):
+        infected = {}
+        s = [0]*50
+
+        for i in range(len(inf_vectors)):
+            v = inf_vectors[i]
+            for r in range(v.shape[0]):
+                for c in range(v.shape[1]):
+                    loc = city_list[c]
+                    state = loc.split(',')[1]
+                    if v[r,c] > 0:
+                        if state in infected:
+                            infected[state] += v[r,c]
+                        else:
+                            infected[state] = v[r,c]
+
+        for state in infected:
+            index = states.index(state)
+            s[index] = infected[state]
+
+        return s
+
+    @staticmethod
+    def distribute(num, prob_dist):
+        num = int(num)
+        cum_sum = np.cumsum(prob_dist)
+        dist = np.zeros(len(prob_dist))
+        for i in range(num):
+            r = random.random()
+            for j in range(len(cum_sum)):
+                if cum_sum[j] >= r:
+                    dist[j] += 1
+                    break
+        return dist
+
+    @staticmethod
+    def det_distribute(num, prob_dist):
+        dist = num*prob_dist
+
+        return dist
+
+
+    @staticmethod
+    def init_vectors(max_pd, num_cities):
+        V = []
+        for i in range(1,max_pd+1):
+            V.append( np.zeros((i,num_cities)) )
+
+        return V
+
+    @staticmethod
+    def infect_indiv(p):
+        rand = random.random()
+        return rand <= p
+
+    @staticmethod
+    def seasonal_prob(p, t):
+        a = .6
+        omega = .017
+        phi = 1.87
+        lamda = .55
+        return p * ((1-a)+a*math.pow(math.fabs(math.sin(omega*t+phi)),lamda))
+
+
+    @staticmethod
+    def propagate(n, city, V, prob, contacts, max_lat, tstep):
+        p = Util.seasonal_prob(prob, tstep)
+        for i in range(int(n)):
+            c = contacts[ random.randint(0,len(contacts)-1) ]
+            for i in range(c):
+                if Util.infect_indiv(p):
+                    pd = random.randint(0,max_lat-1)
+                    V[pd][city] += 1
+        return V
+
+    @staticmethod
+    def determine_inf_pd(infected, V, max_inf):
+        for i in range(len(infected)):
+            num = int(infected[i])
+            if num > 0:
+                for j in range(num):
+                    pd = random.randint(0, max_inf-1)
+                    V[pd][i] += 1
+
+        return V
+
+
+

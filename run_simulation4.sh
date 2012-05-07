26dc979bd5d9a262034c6e84c330cf021eb4d262
Kris Samala <mksamala@gmail.com>
2012-05-07 00:22:34 -0400

    50 runs per value

---
+++ run_simulation4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+count=40
+for p in `seq 0.019 .001 .022`; do
+    for t in `seq 1 1 50`; do
+        ./runCmd -c "python faster_sim3.py 10000000 $p out/contact_dist.out citydata/states.p citydata/census.p out/trans_prob.csv out/city_list.p results2/matrix$count-$t" -e err2/out$count-$t.err -o output2/out$count-$t --nowait
+    done
+    let count+=1
+done

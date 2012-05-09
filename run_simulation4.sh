6cdef9534edacbd292d53da4000e1e97d0f902cc
Kris Samala <mksamala@gmail.com>
2012-05-09 12:16:42 -0400

    change p values

--- run_simulation4.sh
+++ run_simulation4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 count=40
-for p in `seq 0.019 .001 .022`; do
+for p in `seq 0.01 .001 .03`; do
     for t in `seq 1 1 50`; do
         ./runCmd -c "python faster_sim3.py 300000000 $p out/contact_dist.out citydata/states.p citydata/census.p out/trans_prob.csv out/city_list.p google_data/50states04-12.csv results3/matrix$count-$t" -e err3/out$count-$t.err -o output3/out$count-$t --nowait
     done

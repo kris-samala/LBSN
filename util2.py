03df4edf889936f090c76e27e9bb8b4cd5ef646b
Kris Samala <mksamala@gmail.com>
2012-05-13 23:21:37 -0400

    index one off

--- util2.py
+++ util2.py
@@ -88,7 +88,7 @@ class Util:
             num = int(infected[i])
             if num > 0:
                 for j in range(num):
-                    pd = random.choice(inf_dist)
+                    pd = random.choice(inf_dist) - 1
                     V[pd][i] += 1
 
         return V

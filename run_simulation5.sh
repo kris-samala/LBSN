166fdc60c5117c3fddd48890d7a4d1d9075c8a7f
Kris Samala <mksamala@gmail.com>
2012-05-17 04:30:30 -0400

    revert to gowalla contact dist

---
+++ run_simulation5.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+count=0
+for p in `seq 0.019 .001 .025`; do
+    for t in `seq 1 1 10`; do
+        ./runCmd -c "python faster_sim5.py 300000000 $p out/school_contacts.p citydata/states.p citydata/census.p out/trans_prob.csv out/city_list.p google_data/50states04-12.csv results5/matrix$count-$t" -e err5/out$count-$t.err -o output5/out$count-$t --nowait
+    done
+    let count+=1
+done

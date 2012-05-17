fe413dc939dc98c4ad6f7597e463a23f610c1e4d
Kris Samala <mksamala@gmail.com>
2012-05-17 04:37:31 -0400

    fixed input contact_dist

--- run_simulation5.sh
+++ run_simulation5.sh
@@ -3,7 +3,7 @@
 count=0
 for p in `seq 0.019 .001 .025`; do
     for t in `seq 1 1 10`; do
-        ./runCmd -c "python faster_sim5.py 300000000 $p out/school_contacts.p citydata/states.p citydata/census.p out/trans_prob.csv out/city_list.p google_data/50states04-12.csv results5/matrix$count-$t" -e err5/out$count-$t.err -o output5/out$count-$t --nowait
+        ./runCmd -c "python faster_sim5.py 300000000 $p out/contact_dist.out citydata/states.p citydata/census.p out/trans_prob.csv out/city_list.p google_data/50states04-12.csv results5/matrix$count-$t" -e err5/out$count-$t.err -o output5/out$count-$t --nowait
     done
     let count+=1
 done

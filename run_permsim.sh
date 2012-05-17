55c7b8801ea3df5bb71c0dcd8c3c53f2f49f450e
Kris Samala <mksamala@gmail.com>
2012-05-16 21:27:20 -0400

    permuted T

---
+++ run_permsim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+count=0
+for p in `seq 0.019 .001 .022`; do
+    for t in `seq 1 1 50`; do
+        ./runCmd -c "python permute_sim.py 300000000 $p out/school_contacts.p citydata/states.p citydata/census.p out/trans_prob.csv out/city_list.p google_data/50states04-12.csv permresults/matrix$count-$t" -e permerr/out$count-$t.err -o permoutput/out$count-$t --nowait
+    done
+    let count+=1
+done

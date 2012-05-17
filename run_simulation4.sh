80e74fc2ecd39eb412d1cc64a6f963811f4bfde8
Kris Samala <mksamala@gmail.com>
2012-05-17 00:18:39 -0400

    breakpoint at 500000

--- run_simulation4.sh
+++ run_simulation4.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-count=40
+count=0
 for p in `seq 0.019 .001 .022`; do
     for t in `seq 1 1 50`; do
-        ./runCmd -c "python faster_sim3.py 300000000 $p out/school_contacts.p citydata/states.p citydata/census.p out/trans_prob.csv out/city_list.p google_data/50states04-12.csv results4/matrix$count-$t" -e err4/out$count-$t.err -o output4/out$count-$t --nowait
+        ./runCmd -c "python faster_sim4.py 300000000 $p out/school_contacts.p citydata/states.p citydata/census.p out/trans_prob.csv out/city_list.p google_data/50states04-12.csv results4/matrix$count-$t" -e err4/out$count-$t.err -o output4/out$count-$t --nowait
     done
     let count+=1
 done
